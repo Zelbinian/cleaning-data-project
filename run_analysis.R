cdd64732ede084b61ec34368fadc2d28e66c617d
Dustin Hodge <dlhodge@uw.edu>
2015-07-22 09:52:45 -0700

    Assigned the variable names to the data

--- run_analysis.R
+++ run_analysis.R
@@ -3,3 +3,10 @@
 varNames <- read.table("features.txt", header = F)
 testData <- read.table("test/X_test.txt", header = F)
 trainData <- read.table("train/X_train.txt", header = F)
+
+# prepare the varNames data to be used as column headers
+varNames <- t(varNames[,2])
+
+# assign the names to be the column headers
+names(testData) <- varNames
+names(trainData) <- varNames
