db4045f31d51a1b0ae69166e7b30467106b6a9b8
Dustin Hodge <dlhodge@uw.edu>
2015-07-22 09:21:24 -0700

    Loading data files
    The key here is to use sep="" to make R read in the proper number of
    columns

---
+++ run_analysis.R
@@ -0,0 +1,3 @@
+features <- read.delim("features.txt", sep = "", header = F)
+testData <- read.delim("test/X_test.txt", sep = "", header = F)
+trainData <- read.delim("train/X_train.txt", sep = "", header = F)
