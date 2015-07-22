ae59337e1d559ec224d808cd06c0c6bc69533439
Dustin Hodge <dlhodge@uw.edu>
2015-07-22 11:56:50 -0700

    Lots of changes
    Forgot to make individual commits, so the changes are:
    1. We don't actually need varNames data for anything, so loading,
    processing, and setting the column names in one action to be more
    efficient
    2. Creating a subset of the data that is just the mean and std dev
    columns

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,23 @@
-# load in each of the data sets; they don't have headers, so header = F is necessary
-
-varNames <- read.table("features.txt", header = F)
+# load in in the raw data sets; they don't have headers, so overriding the default
 testData <- read.table("test/X_test.txt", header = F)
 trainData <- read.table("train/X_train.txt", header = F)
 
-# prepare the varNames data to be used as column headers
-varNames <- t(varNames[,2])
+# combining them into one data frame
+fullData <- rbind(testData, trainData)
 
-# assign the names to be the column headers
-names(testData) <- varNames
-names(trainData) <- varNames
+# the column headers are stored in the features file
+# this reads it in, and then sets that column to be the column names for the data
+# this skips storing any of that in an additional variable or two because they're not
+# necessary for any other computations
+names(fullData) <- t(read.table("features.txt", header = F)[,2])
+
+# Now to reduce the dataset to just the columns for means and std dev.
+# The codebook that comes with the data says that the variable names are annotated
+# with "mean()" and "std()", respectively
+
+# first, using grep to get the columns we want
+meanCols <- grep("mean\\(\\)", names(fullData))
+stdCols <- grep("std\\(\\)", names(fullData))
+
+# putting them together into one vector and then subsetting with it
+meansAndStds <- fullData[, c(meanCols, stdCols)]
